d85a5b66b7f74ead4f83e1c19d6def7147046ccd
Huffer342-WSH <718007138@qq.com>
2024-11-19 21:17:26 +0800

    [fix]: 删除无用的类型定义

--- Source/fixed_point/Include/radar_math_types.h
+++ Source/fixed_point/Include/radar_math_types.h
@@ -22,14 +22,6 @@
 
 typedef float rd_float_t;
 
-typedef int8_t q7_t;
-typedef int16_t q15_t;
-typedef int32_t q31_t;
-typedef int64_t q63_t;
-typedef uint8_t uq7_t;
-typedef uint16_t uq16_t;
-typedef uint32_t uq32_t;
-typedef uint64_t uq64_t;
 
 #include "radar_matrix.h"
 
